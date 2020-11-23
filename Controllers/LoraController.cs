98951d347ea8ec556485a576ea548edab02fee62
zbabac <zlatko.babic@outlook.com>
2020-11-23 17:46:22 +0100

    Add project files.

---
+++ Controllers/LoraController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MySqlConnector;
+using static WebAppJson.Models.SensorData;
+using static WebAppJson.Models.SensorData.MysqlDataContext;
+
+namespace WebAppJson.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoraController : ControllerBase
+    {
+        /*private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };*/
+        
+        
+        private readonly ILogger<LoraController> _logger;
+
+        public LoraController(ILogger<LoraController> logger)
+        {
+            _logger = logger;
+        }
+        /*
+        public async Task InsertAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"INSERT INTO `sensors.history` (`devid`, `time`, `lat`, `lon`, `alt`, `rssi`, `snr`, `freq`, `port`, `payload_hex`) VALUES (@devid, @time, @lat, @lon, @alt, @rssi, @snr, @freq, @port, @payload_hex);";
+            BindParams(cmd);
+            await cmd.ExecuteNonQueryAsync();
+            //Id = (int)cmd.LastInsertedId;
+        }*/
+
+        [HttpPost]
+        public SensorOutput JsonFromServer([FromBody] SensorOutput jsonBody)
+        {
+            // Get data from POST body sent by Lora server, only subset defined in the class SensorOutput is imported.
+            // This class will be inserted in DB.
+            if (MysqlDataContext.GetOneSensor(jsonBody.devid) is null)
+            {
+                MysqlDataContext.InsertNewSensorId(jsonBody);
+                MysqlDataContext.InsertSensorHistory(jsonBody);
+            }
+            else
+            {
+                MysqlDataContext.InsertSensorHistory(jsonBody);
+                MysqlDataContext.UpdateSensorTime(jsonBody);
+            }
+            return jsonBody;
+        }
+
+        /*
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }*/
+    }
+}
---
+++ Models/SensorData.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MySqlConnector;
+
+namespace WebAppJson.Models
+{
+    public class SensorData
+    {
+        public class SensorOutput
+        {
+            [JsonPropertyName("DevEUI")]
+            public string devid { get; set; }
+            [JsonPropertyName("DevLocTime")]
+            public DateTime time { get; set; }
+
+            [JsonPropertyName("Channel")]
+            public string channel { get; set; }
+            [JsonPropertyName("DevLAT")]
+            public float lat { get; set; }
+            [JsonPropertyName("DevLON")]
+            public float lon { get; set; }
+            [JsonPropertyName("DevALT")]
+            public float alt { get; set; }
+            [JsonPropertyName("LrrRSSI")]
+            public float rssi { get; set; }
+            [JsonPropertyName("LrrSNR")]
+            public float snr { get; set; }
+            [JsonPropertyName("Frequency")]
+            public float freq { get; set; }
+            [JsonPropertyName("FPort")]
+            public int port { get; set; }
+            public string payload_hex { get; set; }
+        }
+        public class MysqlDataContext
+        {
+            static string ConnectionString { get; set; }
+
+            public MysqlDataContext(string connectionString)
+            {
+                ConnectionString = connectionString;
+            }
+
+            static MySqlConnection GetConnection()
+            {
+                return new MySqlConnection(ConnectionString);
+            }
+
+            public static SensorOutput GetOneSensor(string DevId)
+            {
+                SensorOutput sensorData = new SensorOutput();
+                //
+                MySqlConnection conn = GetConnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from sensors.sensors where devid = '"+DevId+"'", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                        while (reader.Read())
+                        {
+                            sensorData.devid = Convert.ToString(reader["devid"]);
+                            sensorData.time = Convert.ToDateTime(reader["time"]);
+                            sensorData.lat = (float)reader["lat"];
+                            sensorData.lon = (float)reader["lon"];
+                            sensorData.alt = (float)reader["alt"];
+                            sensorData.channel = Convert.ToString(reader["channel"]);
+                            sensorData.rssi = (float)reader["rssi"];
+                            sensorData.snr = (float)reader["snr"];
+                            sensorData.freq = (float)reader["alt"];
+                            sensorData.port = (int)reader["port"];
+                            sensorData.payload_hex = Convert.ToString(reader["payload_hex"]);
+                        }
+                    
+                }
+                return sensorData;
+            }
+
+            public static async Task InsertSensorHistory(SensorOutput sensorInput)
+            {
+                using (MySqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO `sensors.history` (`devid`, `time`, `lat`, `lon`, `alt`, `rssi`, `snr`, `freq`, `port`, `payload_hex`)" + 
+                        "VALUES ("+sensorInput.devid+", "+sensorInput.time+", "+sensorInput.lat+", "+sensorInput.lon+", "+sensorInput.alt+", "+sensorInput.channel+
+                        ", "+sensorInput.rssi+", "+sensorInput.snr+", "+sensorInput.freq+", "+sensorInput.freq+", "+sensorInput.port+", "+sensorInput.payload_hex+");", conn);
+                    
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            public static async Task InsertNewSensorId(SensorOutput sensorInput)
+            {
+                using (MySqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO `sensors.sensors` (`devid`, `tennantid`, `name`, `Last_time`, `lat`, `lon`, `alt`, `rssi`, `snr`, `freq`, `port`, `payload_hex`)" +
+                        "VALUES (" + sensorInput.devid + ", '1', " + sensorInput.devid + ", " + sensorInput.time + ", " + sensorInput.lat + ", " + sensorInput.lon + ", " +
+                        sensorInput.alt + ", " + sensorInput.channel + ", " + sensorInput.rssi + ", " + sensorInput.snr + ", " + sensorInput.freq + ", " + sensorInput.freq + ", " + sensorInput.port + ", " + sensorInput.payload_hex + ");", conn);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            public static async Task UpdateSensorTime(SensorOutput sensorInput)
+            {
+                using (MySqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("UPDATE `sensors.sensors` SET last_time='"+sensorInput.time+"' WHERE devid='"+sensorInput.devid+"';", conn);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            public static async Task UpdateSensorNameTennant(string devid, string name, int tennantid)
+            {
+                using (MySqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("UPDATE `sensors.sensors` SET name='" + name + "', tennantid='"+tennantid+ "' WHERE devid='" + devid + "';", conn);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
